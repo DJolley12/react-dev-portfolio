680ed045a05f86a5e43fcecb7f565512a759aabc
dcjolley12 <dcjolley12@gmail.com>
2020-10-10 18:20:03 -0600

    Add href to IconSection

--- src/components/Footer/FooterContents/IconSection.js
+++ src/components/Footer/FooterContents/IconSection.js
@@ -20,8 +20,17 @@ function IconSection() {
       >
         <i className="icon_list nes-icon github is-large flicker"></i>
       </a>
-      <i className="icon_list nes-icon linkedin is-large d-flex justify-content-lg-center flicker"></i>
-      <i className="icon_list nes-icon gmail is-large d-flex justify-content-lg-center flicker"></i>
+      <a
+          href="https://www.linkedin.com/in/daniel-jolley-06947b1b5/"
+          className="justify-content-lg-center"       
+        >
+        <i className="icon_list nes-icon linkedin is-large d-flex flicker"></i>
+      </a>
+      <a
+        className="d-flex justify-content-lg-center"
+      >
+        <i className="icon_list nes-icon gmail is-large  flicker"></i>
+      </a>
     </section>
   );
 }
