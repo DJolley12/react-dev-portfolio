46a4e6afa2c3897897481f8ea808169e9718b1f5
dcjolley12 <dcjolley12@gmail.com>
2020-10-27 21:58:00 -0600

    Updates

---
+++ precache-manifest.9a8df5e444309797af05ae3536c638a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6502ab36976dcefdd702ee6ef15b8c4a",
+    "url": "/react-dev-portfolio/index.html"
+  },
+  {
+    "revision": "97cce5e8842873d087fb",
+    "url": "/react-dev-portfolio/static/css/main.8c806d06.chunk.css"
+  },
+  {
+    "revision": "4ce6e49343ed5a179a35",
+    "url": "/react-dev-portfolio/static/js/2.0a106ee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dev-portfolio/static/js/2.0a106ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cce5e8842873d087fb",
+    "url": "/react-dev-portfolio/static/js/main.09a40a7a.chunk.js"
+  },
+  {
+    "revision": "064b2b626227252341d3",
+    "url": "/react-dev-portfolio/static/js/runtime-main.ff3d9ddc.js"
+  },
+  {
+    "revision": "0f154d69b288820b8e22d681d6767e9c",
+    "url": "/react-dev-portfolio/static/media/waneella_pink_city.0f154d69.gif"
+  }
+]);
